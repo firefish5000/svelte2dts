3ba8a2d5ffb004d26ca6c377e74eb0604ab662fd
firefish5000 <1813610+firefish5000@users.noreply.github.com>
2020-11-28 16:40:35 -0600

    Use ts.parseCommandLine.
    Also prepare to run old tests on new code to suggest version number
    and to better identify backward incompatible changes

--- utils/suggestVersion.sh
+++ utils/suggestVersion.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 set -e
 IS_PRE=""
 for flag in "$@" ; do
@@ -9,22 +10,25 @@ done
 IS_CLEAN="$(git diff-index --quiet HEAD; echo $?)"
 
 if [[ $IS_CLEAN != 0 ]]; then 
-  echo "Working directory is not clean. Refusing to proceed!" > &2
+  >&2 echo "Working directory is not clean. Refusing to proceed!" 
   exit 1
 else
   OLD_TAG="$(git describe --tags "$(git rev-list --tags --max-count=1)")"
-  TEST_DIFFER="$(git diff-index --quiet ${OLD_TAG}; echo $?)"
+  TEST_DIFFER="$(git diff-index --quiet ${OLD_TAG} -- test jest.conf.js; echo $?)"
+  if [[ $TEST_DIFFER != 0 ]]; then
+    >&2 echo "Tests differ!"
+  fi
   git checkout "${OLD_TAG}" -- test jest.config.js
   TEST_STATUS="$(npm test; echo $?)"
   OLD_VERSION=OLD_TAG
   if [[ $TEST_STATUS != 0 ]]; then
-    echo "It does not work with old tests from tag ${OLD_TAG}! Suggesting a major bump!" > &2
+    >&2 echo "It does not work with old tests from tag ${OLD_TAG}! Suggesting a major bump!"
     npm version "${IS_PRE:+pre}"major
   elif [[ $TEST_DIFFER != 0 ]]; then
-    echo "Tests have changed since tag ${OLD_TAG}! Suggesting a minor bump!" > &2
+    >&2 echo "Tests have changed since tag ${OLD_TAG}! Suggesting a minor bump!"
     npm version "${IS_PRE:+pre}"minor
   else
-    echo "We have the same tests as tag ${OLD_TAG}! Suggesting a patch bump!" > &2
+    >&2 echo "We have the same tests as tag ${OLD_TAG}! Suggesting a patch bump!"
     npm version "${IS_PRE:+pre}"patch
   fi
   git checkout -- ./
