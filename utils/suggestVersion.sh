7f865d96693c27ebbc9c45c6a3fe6658092163a0
firefish5000 <1813610+firefish5000@users.noreply.github.com>
2020-11-28 17:22:36 -0600

    Add back testing

--- utils/suggestVersion.sh
+++ utils/suggestVersion.sh
@@ -12,24 +12,32 @@ IS_CLEAN="$(git diff-index --quiet HEAD; echo $?)"
 if [[ $IS_CLEAN != 0 ]]; then 
   >&2 echo "Working directory is not clean. Refusing to proceed!" 
   exit 1
-else
-  OLD_TAG="$(git describe --tags "$(git rev-list --tags --max-count=1)")"
-  TEST_DIFFER="$(git diff-index --quiet ${OLD_TAG} -- test jest.conf.js; echo $?)"
-  if [[ $TEST_DIFFER != 0 ]]; then
-    >&2 echo "Tests differ!"
-  fi
-  git checkout "${OLD_TAG}" -- test jest.config.js
-  TEST_STATUS="$(npm test; echo $?)"
-  OLD_VERSION=OLD_TAG
-  if [[ $TEST_STATUS != 0 ]]; then
-    >&2 echo "It does not work with old tests from tag ${OLD_TAG}! Suggesting a major bump!"
-    npm version "${IS_PRE:+pre}"major
-  elif [[ $TEST_DIFFER != 0 ]]; then
-    >&2 echo "Tests have changed since tag ${OLD_TAG}! Suggesting a minor bump!"
-    npm version "${IS_PRE:+pre}"minor
-  else
-    >&2 echo "We have the same tests as tag ${OLD_TAG}! Suggesting a patch bump!"
-    npm version "${IS_PRE:+pre}"patch
-  fi
-  git checkout -- ./
 fi
+>&2 echo "Working directory is clean. Proceeding!" 
+IS_FAILING="$(npx jest --ci --bail --useStderr; echo $?)"
+if [[ $IS_FAILING != 0 ]]; then 
+  >&2 echo "Working directory tests are failing. Refusing to proceed!" 
+  exit 1
+fi
+>&2 echo "Working directory tests are passing. Proceeding!" 
+
+
+OLD_TAG="$(git describe --tags "$(git rev-list --tags --max-count=1)")"
+TEST_DIFFER="$(git diff-index --quiet ${OLD_TAG} -- test jest.conf.js; echo $?)"
+if [[ $TEST_DIFFER != 0 ]]; then
+  >&2 echo "Tests differ!"
+fi
+git checkout "${OLD_TAG}" -- test jest.config.js
+TEST_STATUS="$(npx jest --ci --bail --useStderr; echo $?)"
+OLD_VERSION=OLD_TAG
+if [[ $TEST_STATUS != 0 ]]; then
+  >&2 echo "We fail old tests from tag ${OLD_TAG}. Suggesting a major bump!"
+  echo 'major'
+elif [[ $TEST_DIFFER != 0 ]]; then
+  >&2 echo "Tests have changed since tag ${OLD_TAG}. Suggesting a minor bump!"
+  echo 'minor'
+else
+  >&2 echo "We have the same tests as tag ${OLD_TAG}. Suggesting a patch bump!"
+  echo 'patch'
+fi
+git checkout -- ./
